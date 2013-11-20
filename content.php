e451845251118cffb8ea3d03ddbc7f81ff36b5d9
Bearded Avenger <email@nickhaskins.com>
2013-11-20 17:21:39 -0600

    core vars, build up post

--- content.php
+++ content.php
@@ -6,10 +6,12 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-	<?php
-	the_date();
-
-	the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'soren' ) ); 
-	?>
+	<time class="soren-entry-date" datetime="<?php the_date('F jS, Y'); ?>" itemprop="datePublished" pubdate><?php echo the_time('F jS, Y'); ?></time>
+	
+	<h2 class="soren-entry-title" itemprop="title"><a href="<?php the_permalink();?>"><?php the_title();?></a></h2>
+	
+	<section class="soren-entry-content">
+		<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'soren' ) ); ?>
+	</section>
 
 </article><!-- #post-## -->
