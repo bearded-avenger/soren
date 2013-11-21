a8b48c0fca6a35c8528206db0c1738df2e232944
Bearded Avenger <email@nickhaskins.com>
2013-11-21 13:05:47 -0600

    do body fader

--- inc/scripts.php
+++ inc/scripts.php
@@ -12,6 +12,7 @@ class sorenScriptLoader {
 
 		wp_enqueue_script('jquery');
 		wp_enqueue_style('soren-style', 		SOREN_THEME_URL.'/less/style.less', SOREN_THEME_VERSION, true);
+		wp_enqueue_script('soren-script', 		SOREN_THEME_URL.'/js/soren.min.js', SOREN_THEME_VERSION, true);
 
 		// push nav
 		wp_register_script('soren-menu', 			SOREN_THEME_URL.'/js/pushy.min.js', true);
---
+++ js/soren.js
@@ -0,0 +1,27 @@
+jQuery(document).ready(function() {
+
+jQuery('body').css('display', 'none');
+
+jQuery('body').fadeIn(500);
+
+
+
+jQuery('nav a').click(function(event) {
+
+event.preventDefault();
+
+newLocation = this.href;
+
+jQuery('body').fadeOut(400, newpage);
+
+});
+
+
+
+function newpage() {
+
+window.location = newLocation;
+
+}
+
+});
