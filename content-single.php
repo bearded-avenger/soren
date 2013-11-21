e3b775c17faefef884c226cdfded6f74852ceb0c
Bearded Avenger <email@nickhaskins.com>
2013-11-21 13:13:33 -0600

    do faders on links

--- content-single.php
+++ content-single.php
@@ -8,7 +8,7 @@
 
 	<time class="soren-entry-date" datetime="<?php the_date('F jS, Y'); ?>" itemprop="datePublished" pubdate><?php echo the_time('F jS, Y'); ?></time>
 
-	<h2 class="soren-entry-title" itemprop="title"><a href="<?php the_permalink();?>"><?php the_title();?></a></h2>
+	<h2 class="soren-entry-title" itemprop="title"><a class="soren-fader" href="<?php the_permalink();?>"><?php the_title();?></a></h2>
 
 	<section class="soren-entry-content">
 		<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'soren' ) ); ?>
