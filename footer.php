9843e325b2a37a8349ea9bc4063696bee975ddf3
Bearded Avenger <email@nickhaskins.com>
2013-11-21 11:55:22 -0600

    do creds

--- footer.php
+++ footer.php
@@ -1,6 +1,10 @@
 </main>
 <footer class="soren-content push">
-<p>Footer</p>
+	<section class="soren-creds center">
+		<a href="http://wordpress.org/" rel="generator"><?php printf( __( 'Proudly powered by %s', 'soren' ), 'WordPress' ); ?></a>
+		<span class="sep"> | </span>
+		<?php printf( __( 'Theme: %s', 'flyte' ),'<a href="https://github.com/bearded-avenger/soren" rel="designer">Soren</a>' ); ?>
+	</section>
 </footer>
 
 <?php wp_footer(); ?>
