851585a5d236f1cf9b908c269bf50e60b21d0e11
Bearded Avenger <email@nickhaskins.com>
2013-11-21 17:47:50 -0600

    add comments icon

--- inc/components/component-post-shares.php
+++ inc/components/component-post-shares.php
@@ -10,12 +10,13 @@
 */
 if (!function_exists('soren_post_shares')):
 
-	function soren_post_shares(){
+	function soren_post_shares($togglecomment = false){
 
 		$twitter = sprintf('<a href="http://twitter.com/share?text=%s&amp;url=%s" target="_blank"><i class="sorencon sorencon-twitter"></i></a>', get_permalink(),get_permalink());
 		$facebook = sprintf('<a href="http://www.facebook.com/sharer.php?u=%s&amp;t=%s" target="_blank"><i class="sorencon sorencon-facebook"></i></a>', get_permalink(),get_the_title());
-		
-		$out = sprintf('<section class="soren-post-shares">%s%s</section>',$twitter, $facebook);
+		$togglecomment = sprintf('<a class="show-soren-comments" href="#"><i class="sorencon sorencon-comments"></i></a>');
+
+		$out = true == $togglecomment ? sprintf('<section class="soren-post-shares">%s%s%s</section>',$twitter, $facebook, $togglecomment) : sprintf('<section class="soren-post-shares">%s%s</section>',$twitter, $facebook);
 		return apply_filters('soren_post_share_output', $out);
 	}
 
