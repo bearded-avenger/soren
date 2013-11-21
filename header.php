436f251b9d97a8afdaa4e4cd308077c92c29441f
Bearded Avenger <email@nickhaskins.com>
2013-11-21 11:35:49 -0600

    we actually need to load the font from google duh

--- header.php
+++ header.php
@@ -7,7 +7,7 @@
 <title><?php wp_title( '|', true, 'right' ); ?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-
+<link href='//fonts.googleapis.com/css?family=Open+Sans:300,800,400&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
 <?php wp_head(); ?>
 </head>
 <?php do_action('soren_after_header'); //action ?>
