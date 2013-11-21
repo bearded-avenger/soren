4a012bd0488174a200cb426ca08e2a799b954ddb
Bearded Avenger <email@nickhaskins.com>
2013-11-21 16:08:03 -0600

    sharing

--- inc/components/component-post-shares.php
+++ inc/components/component-post-shares.php
@@ -12,8 +12,8 @@ if (!function_exists('soren_post_shares')):
 
 	function soren_post_shares(){
 
-		$twitter = sprintf('<a href="http://twitter.com/share?text=%s&amp;url=%s"><i class="sorencon sorencon-twitter-square"></i></a>', get_permalink(),get_permalink());
-		$facebook = sprintf('<a href="http://www.facebook.com/sharer.php?u=%s&amp;t=%s"><i class="sorencon sorencon-facebook-square"></i></a>', get_permalink(),get_the_title());
+		$twitter = sprintf('<a href="http://twitter.com/share?text=%s&amp;url=%s" target="_blank"><i class="sorencon sorencon-twitter"></i></a>', get_permalink(),get_permalink());
+		$facebook = sprintf('<a href="http://www.facebook.com/sharer.php?u=%s&amp;t=%s" target="_blank"><i class="sorencon sorencon-facebook"></i></a>', get_permalink(),get_the_title());
 		
 		$out = sprintf('<section class="soren-post-shares">%s%s</section>',$twitter, $facebook);
 		return apply_filters('soren_post_share_output', $out);
