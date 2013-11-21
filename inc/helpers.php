cf05f5965e816cd997bf5ccd7e7ebae14d026019
Bearded Avenger <email@nickhaskins.com>
2013-11-21 11:17:19 -0600

    better formatting

--- inc/helpers.php
+++ inc/helpers.php
@@ -4,14 +4,11 @@
   	* Adds schema to the thml tag depending on the current page view
   	*
   	* @author  Nick Haskins <email@nickhaskins.com>
-  	*
   	* @since 1.0
   	*
 */
-if (!function_exists('soren_html_schema')):
+if (!function_exists('soren_html_schema')){
 	function soren_html_schema(){
-
-	 	$base = 'http://schema.org/';
 
 	 	$type = '';
 	    switch ($type):
@@ -37,35 +34,35 @@ if (!function_exists('soren_html_schema')):
 
 	    endswitch;
 
-	    echo 'itemscope="itemscope" itemtype="' . $base . $type . '"';
+	    echo 'itemscope="itemscope" itemtype="http://schema.org'.$type.'"';
 	}
-endif;
+}
+
 
 /**
   	* Provides a fallback for the push nav
   	*
   	* @author  Nick Haskins <email@nickhaskins.com>
-  	*
   	* @since 1.0
   	*
 */
-if (!function_exists('soren_nav_fallback')):
+if (!function_exists('soren_nav_fallback')){}
 	function soren_nav_fallback(){
 		printf('<nav class="pushy push-left"><ul id="soren_nav_fallback" class="unstyled">%s</ul></nav>', wp_list_pages( 'title_li=&sort_column=menu_order&depth=1&echo=0') );
 	}
-endif;
+}
+
 
 /**
   	* Used within the soren_gallery function, to search the post for post ids
   	*
   	* @author  Nick Haskins <email@nickhaskins.com>
-  	*
   	* @since 1.0
   	*
 */
-if (!function_exists('soren_gallery_match')):
+if (!function_exists('soren_gallery_match')){}
     function soren_gallery_match( $regex, $content ) {
         preg_match($regex, $content, $matches);
         return $matches[1];
     }
-endif;
+}
