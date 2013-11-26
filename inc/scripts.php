a5e025ada4304d6ffe8a3e0de5c4d9688c51e8b0
Bearded Avenger <email@nickhaskins.com>
2013-11-26 08:33:05 -0600

    minify new

--- inc/scripts.php
+++ inc/scripts.php
@@ -12,7 +12,7 @@ class sorenScriptLoader {
 
 		wp_enqueue_script('jquery');
 		wp_enqueue_style('soren-style', 		SOREN_THEME_URL.'/less/style.less', SOREN_THEME_VERSION, true);
-		wp_enqueue_script('soren-script', 		SOREN_THEME_URL.'/js/soren.js', array('jquery'),SOREN_THEME_VERSION, true);
+		wp_enqueue_script('soren-script', 		SOREN_THEME_URL.'/js/soren.min.js', array('jquery'),SOREN_THEME_VERSION, true);
 
 		// push nav
 		wp_register_script('soren-menu', 			SOREN_THEME_URL.'/js/pushy.min.js', true);
