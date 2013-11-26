40b16d65921e2b4b27eb85252a0363d41dac3ec2
Bearded Avenger <email@nickhaskins.com>
2013-11-26 12:02:09 -0600

    bootstrap modal to core, $clean arg for post author

--- inc/components/component-post-author.php
+++ inc/components/component-post-author.php
@@ -10,7 +10,7 @@
 */
 if (!function_exists('soren_post_author')):
 
-	function soren_post_author($extended = '', $avatarsize = 80, $link = false){
+	function soren_post_author($extended = '', $avatarsize = 80, $link = false, $clean = false){
 
         global $post;
         setup_postdata($post);
@@ -29,33 +29,51 @@ if (!function_exists('soren_post_author')):
 
         $author = (true == $link) ? sprintf('<h6 class="media-heading"><a href="%s">%s</a></h6>', $author_url, $author_name) : sprintf('<h6 class="media-heading">%s</h6>', $author_name);
 
-		?>
-        <section class="media soren-post-author">
-            <div class="pull-left">
-                <a class="thumbnail" href="<?php echo $author_out_link; ?>" target="_blank">
-                    <?php echo get_avatar( $author_email, $avatarsize, $default_avatar); ?>
-                </a>
-            </div>
-            <div class="media-body">
-            	<?php if (true == $extended) { ?>
-	            	<div class="row">
-	            		<div class="col-md-8 soren-author-details-wrap">
-	                		<small class="soren-author-note"><?php _e('Posted by ', 'soren');?></small>
-	                		<?php echo $author;?>
-	                		<p class="soren-author-details"><?php echo $author_desc; ?></p>
-	                	</div>
-	                	<div class="col-md-4 soren-author-extended-wrap">
-		                	<?php if( $author_out_link != '' ) {
-		                    	printf( '<a href="%s" class="btn btn-default soren-author-link" target="_blank">More of %s  &nbsp;&rsaquo;</a> ', $author_out_link, $author_first);
-		                	} ?>
+        if (true == $clean) { ?>
+
+            <a class="thumbnail" href="<?php echo $author_out_link; ?>" target="_blank"><?php echo get_avatar( $author_email, $avatarsize, $default_avatar); ?></a>
+
+        	<?php if (true == $extended) { ?>
+
+        		<small class="soren-author-note"><?php _e('Posted by ', 'soren');?></small>
+        		<?php echo $author;?>
+        		<p class="soren-author-details"><?php echo $author_desc; ?></p>
+            	<?php if( $author_out_link != '' ) {
+                	printf( '<a href="%s" class="btn btn-default soren-author-link" target="_blank">More of %s  &nbsp;&rsaquo;</a> ', $author_out_link, $author_first);
+            	}
+
+            } else { ?>
+                <small class="soren-author-note"><?php _e('Posted by', 'soren');?></small>
+                <?php echo $author;
+            }
+
+        } else { ?>
+	        <section class="media soren-post-author">
+	            <div class="pull-left">
+	                <a class="thumbnail" href="<?php echo $author_out_link; ?>" target="_blank">
+	                    <?php echo get_avatar( $author_email, $avatarsize, $default_avatar); ?>
+	                </a>
+	            </div>
+	            <div class="media-body">
+	            	<?php if (true == $extended) { ?>
+		            	<div class="row">
+		            		<div class="col-md-8 soren-author-details-wrap">
+		                		<small class="soren-author-note"><?php _e('Posted by ', 'soren');?></small>
+		                		<?php echo $author;?>
+		                		<p class="soren-author-details"><?php echo $author_desc; ?></p>
+		                	</div>
+		                	<div class="col-md-4 soren-author-extended-wrap">
+			                	<?php if( $author_out_link != '' ) {
+			                    	printf( '<a href="%s" class="btn btn-default soren-author-link" target="_blank">More of %s  &nbsp;&rsaquo;</a> ', $author_out_link, $author_first);
+			                	} ?>
+			                </div>
 		                </div>
-	                </div>
-                <?php } else { ?>
-	                <small class="soren-author-note"><?php _e('Posted by', 'soren');?></small>
-	                <?php echo $author;?>
-                <?php }?>
-            </div>
-        </section><?php
+	                <?php } else { ?>
+		                <small class="soren-author-note"><?php _e('Posted by', 'soren');?></small>
+		                <?php echo $author;?>
+	                <?php }?>
+	            </div>
+	        </section><?php }
     }
 
 endif;
