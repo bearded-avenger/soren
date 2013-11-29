4376b44badd72631d76328784a45827fb189317b
Bearded Avenger <email@nickhaskins.com>
2013-11-29 16:41:36 -0600

    fix busted post nav

--- inc/components/component-post-nav.php
+++ inc/components/component-post-nav.php
@@ -11,13 +11,13 @@ if (!function_exists('soren_post_nav')) {
 
 	    	if ($adjacent) {
 
-	    		$prev = get_adjacent_post(true,'',false); 
-	    		$prevlink = get_permalink(get_adjacent_post(false,'',false));
+	    		$prev = get_adjacent_post(true,'',true); 
+	    		$prevlink = get_permalink(get_adjacent_post(false,'',true));
 
-				$next = get_adjacent_post(true,'',true);
-				$nextlink = get_permalink(get_adjacent_post(false,'',true));
+				$next = get_adjacent_post(false,'',false);
+				$nextlink = get_permalink(get_adjacent_post(false,'',false));
 
-				if ($prevlink != get_permalink() && !empty($prev) ) {
+				if ($prevlink != get_permalink()) {
 	        		?><a class="soren-post-adjacent previous" href="<?php echo $prevlink;?>">
         				<?php echo get_the_post_thumbnail($prev->ID, array(68,68, true) ); ?>
         				<p><?php _e('Previous Post','soren');?></p>
@@ -25,7 +25,7 @@ if (!function_exists('soren_post_nav')) {
 					</a>
 				<?php }
 
-				if ($nextlink != get_permalink() && !empty($next)) {
+				if ($nextlink != get_permalink()) {
 					?><a class="soren-post-adjacent next" href="<?php echo $nextlink;?>">
 						<?php echo get_the_post_thumbnail($next->ID, array(68,68, true) ); ?>
 						<p><?php _e('Next Post','soren');?></p>
